9d4aaa973bab283e6802685a45e901c02e7fbcee
Denys N <denys@joomla-ua.org>
2018-03-04 23:04:42 +0200

    Update SQL-install/uninstall and add new params

--- admin/uninstall_ckeditor.sql
+++ admin/uninstall_ckeditor.sql
@@ -1,2 +1,7 @@
-DELETE FROM #__extensions WHERE element = 'ckeditor' AND name = 'Editor - CKEditor';
+DELETE FROM
+  #__extensions
+WHERE
+  element = 'ckeditor'
+  AND
+  name = 'Editor - CKEditor';
 -- DELETE FROM   #__components where name='CKEditor Component' AND `option`='com_ckeditor' ;
