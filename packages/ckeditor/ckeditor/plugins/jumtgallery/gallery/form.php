00e76e77f7eba676a4b2d40f46ac425164600183
Denys N <denys@joomla-ua.org>
2018-03-05 05:57:49 +0200

    Fix

--- packages/ckeditor/ckeditor/plugins/jumtgallery/gallery/form.php
+++ packages/ckeditor/ckeditor/plugins/jumtgallery/gallery/form.php
@@ -11,8 +11,8 @@
  **/
 
 define('_JEXEC', 1);
-define('JPATH_BASE', __DIR__ . "/../../../../../..");
-define("MAX_SIZE", "500");
+define('JPATH_BASE', __DIR__ . '/../../../../../..');
+define('MAX_SIZE', '500');
 
 require_once JPATH_BASE . '/includes/defines.php';
 require_once JPATH_BASE . '/includes/framework.php';
